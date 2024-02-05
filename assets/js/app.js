88821a428186da6b9453f7f802f61f86f1bf2f7c
unknown <lyacine230@gmail.com>
2024-02-05 14:13:18 +0100

    changing the color theme from white to dark

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000"
+        "value": "#fff"
       },
       "shape": {
         "type": "circle",
@@ -50,7 +50,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#000",
+        "color": "#fff",
         "opacity": 0.4,
         "width": 1
       },
